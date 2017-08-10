e7824ba4ae217b9b1e330dd9117749eb31c5c91e
Adrian Sirko <cipko@Adrians-MacBook-Air.local>
2017-08-10 18:00:44 -0400

    phase3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,45 @@
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l",
     "m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
+    var lettersGuessed;
 
 var wins = 0;
 var losses = 0;
 var guesses = 10;
 
-    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-    console.log(computerChoice)
+function newGame() { 
+guesses = 10;
+lettersGuessed = [];
+computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log(computerChoice)
 
-document.getElementById("wins").innerHTML = "Wins: " + wins;
-document.getElementById("losses").innerHTML = "losses: " + losses;
-document.getElementById("guesses").innerHTML = "Guesses left: " + guesses;
+}
 
-document.onkeypress = function(event) {
+document.onkeypress = function() {
+    
+
     var userGuess = event.key;
 		
     if(userGuess === computerChoice){
         wins++;
-        alert(wins)
+        var html = 
+        newGame()
     }else{
         guesses--;
+        lettersGuessed.push(userGuess)
         console.log(guesses)
     }
 
     if(guesses === 0){
         losses++
-        alert(losses)
+        newGame()
     }
-
-    
+    var html =
+          "<p>You chose: " + userGuess + "</p>" +
+          "<p>wins: " + wins + "</p>" +
+          "<p>losses: " + losses + "</p>" +
+          "<p>Guesses Left: " + guesses + "</p>" +
+          "<p>Letters Guessed: " + lettersGuessed.join(", ") + "</p>";
+          document.querySelector("#wins").innerHTML = html;
+}
+newGame();
